4ea9c47567034fbcfa91fefc8c8c3e0c5a1217e7
Jonathan Loesch <jonloesch@gmail.com>
2025-06-02 17:11:24 -0400

    whoops

--- packages/core/src/test/signatureBoilerplate.ts
+++ packages/core/src/test/signatureBoilerplate.ts
@@ -1,4 +1,3 @@
-import { PartialDef } from "./core";
 import type { G, MutationState, stopInjection, SpecificDef, MutationWatch, AnyDef } from "./g";
 
 type InjectionParametersBase<Def extends AnyDef> = {
@@ -39,10 +38,6 @@ export type InjectionParameters<Def extends AnyDef, TContext, WatchResult> =
   | InjectionParametersWithWatch<Def, WatchResult>;
 
 export interface Engine {
-  // from<Input, Output, Error>(
-  //   from: G<SpecificDef<Input, Error, Output, unknown, unknown, unknown>>["MutationLocator"]
-  // ): G<SpecificDef<Input, Error, Output, unknown, unknown, unknown>>["MutationLocator"];
-  // into<Output, Input, Error>(from: G<SpecificDef<unknown, unknown, unknown, Input, Error, Output>>["QueryLocator"]): G<SpecificDef<unknown, unknown, unknown, Input, Error, Output>>["QueryLocator"];
   watch: {
     <Input, Output>(ml: G<SpecificDef<Input, unknown, Output, unknown, unknown, unknown>>["MutationLocator"]): MutationWatch<{ input: Input } & MutationState<Output>>;
     <Input, Output, Context>(
