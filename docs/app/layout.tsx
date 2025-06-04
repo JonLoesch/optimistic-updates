260344dbbf61b1d2e17b277a8ff8037347cc2529
Jonathan Loesch <jonloesch@gmail.com>
2025-06-04 14:05:23 -0400

    Fixing up some linting and package issues.  Attempt #5

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { FC, PropsWithChildren } from "react";
 
-const Layout = ({ children }) => {
+const Layout: FC<PropsWithChildren> = ({ children }) => {
   return (
     <div>
       <header>
